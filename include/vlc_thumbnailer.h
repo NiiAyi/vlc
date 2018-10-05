97dc1312c837bf931e45faa654b2d7c40e676e82
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2018-10-05 12:03:11 +0200

    core: Update doc

--- include/vlc_thumbnailer.h
+++ include/vlc_thumbnailer.h
@@ -29,7 +29,7 @@ typedef struct vlc_thumbnailer_t vlc_thumbnailer_t;
 typedef struct vlc_thumbnailer_request_t vlc_thumbnailer_request_t;
 
 /**
- * @brief vlc_thumbnailer_cb defines a callback invoked on thumbnailing completion.
+ * \brief vlc_thumbnailer_cb defines a callback invoked on thumbnailing completion.
  *
  * This callback will always be called, provided vlc_thumbnailer_Request returned
  * a non NULL request.
@@ -37,45 +37,53 @@ typedef struct vlc_thumbnailer_request_t vlc_thumbnailer_request_t;
  * The picture, if any, is owned by the thumbnail, and must be acquired by using
  * \link picture_Hold \endlink to use it passed the callback's scope.
  *
- * @param data Is the opaque pointer passed as vlc_thumbnailer_Create last parameter
+ * \param data Is the opaque pointer passed as vlc_thumbnailer_Create last parameter
+ * \param p_thumbnail The generated thumbnail, or NULL in case of failure
  */
 typedef void(*vlc_thumbnailer_cb)( void* data, picture_t* p_thumbnail );
 
 /**
- * @brief vlc_thumbnailer_Create Creates an asynchrone thumbnailer object
- * @param p_parent A VLC object
- * @return A thumbnailer object, or NULL in case of failure
+ * \brief vlc_thumbnailer_Create Creates a thumbnailer object
+ * \param p_parent A VLC object
+ * \return A thumbnailer object, or NULL in case of failure
  */
 VLC_API vlc_thumbnailer_t*
 vlc_thumbnailer_Create( vlc_object_t* p_parent )
 VLC_USED;
 
 /**
- * @brief vlc_thumbnailer_Request
- * @param p_thumbnailer
- * @param p_input_item
- * @param i_time
- * @param p_cb A callback to be invoked upon completion
- * @param p_data
- * @return
+ * \brief vlc_thumbnailer_Request
+ * \param p_thumbnailer A thumbnailer object
+ * \param p_input_item The input item for which to generate the thumbnail
+ * \param i_time The time at which the thumbnail must be generated
+ * \param p_cb A callback to be invoked upon completion
+ * \param p_data An opaque user-provided value
+ * \return An opaque request object, or NULL in case of failure
  *
- * This function will immediatly start the thumbnailing process.
- * If this function returns a valid thumbnailer object, vlc_thumbnailer_Stop
- * must be called to release the resources, even when the callback gets
- * invoked with an error status.
+ * If this function returns a valid request object, the callback is guaranteed
+ * to be called, even in case of later failure.
+ * The returned request object must not be used after the callback has been
+ * invoked. That object is owned by the thumbnailer, and must not be released.
  */
 VLC_API vlc_thumbnailer_request_t*
 vlc_thumbnailer_Request( vlc_thumbnailer_t *p_thumbnailer,
                          input_item_t* p_input_item, vlc_tick_t i_time,
                          vlc_thumbnailer_cb p_cb, void* p_data );
 
+/**
+ * \brief vlc_thumbnailer_Cancel Cancel a thumbnail request
+ * \param p_thumbnailer A thumbnailer object
+ * \param p_request An opaque thumbnail request object
+ *
+ * The behavior is undefined if the request is cancelled after its completion.
+ */
 VLC_API void
-vlc_thumbnailer_Cancel( vlc_thumbnailer_t* p_thumbnailer, vlc_thumbnailer_request_t* p_request );
+vlc_thumbnailer_Cancel( vlc_thumbnailer_t* p_thumbnailer,
+                        vlc_thumbnailer_request_t* p_request );
 
 /**
- * @brief vlc_thumbnailer_Release releases a thumbnailer and stop all processing
- *        requests, if any.
- * @param p_thumbnailer A thumbnailer object
+ * \brief vlc_thumbnailer_Release releases a thumbnailer and cancel all pending requests
+ * \param p_thumbnailer A thumbnailer object
  */
 VLC_API void vlc_thumbnailer_Release( vlc_thumbnailer_t* p_thumbnailer );
 
